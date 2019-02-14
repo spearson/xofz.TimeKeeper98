071c21dee27ea542e0d60575e967f1084189fe1a
sam <sam@xofz.com>
2019-02-13 19:06:05 -0800

    Slight code cleanup; build for new version of Core98

--- xofz.TimeKeeper98/Framework/PaddedTimeSpanViewer.cs
+++ xofz.TimeKeeper98/Framework/PaddedTimeSpanViewer.cs
@@ -6,10 +6,12 @@
     {
         public virtual string ReadableString(TimeSpan timeSpan)
         {
-            return ((long) timeSpan.TotalHours).ToString().PadLeft(3)
+            return ((long)timeSpan.TotalHours).ToString().PadLeft(3)
                    + "h "
-                   + timeSpan.Minutes.ToString().PadLeft(2) + "m "
-                   + timeSpan.Seconds.ToString().PadLeft(2) + 's';
+                   + timeSpan.Minutes.ToString().PadLeft(2) 
+                   + "m "
+                   + timeSpan.Seconds.ToString().PadLeft(2) 
+                   + 's';
         }
     }
 }
