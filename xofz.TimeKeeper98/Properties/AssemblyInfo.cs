ee01b0906b9e490a64045bddbefd2408dc4f46e4
Sam Pearson <sam@xofz.com>
2021-04-13 06:53:44 -0700

    Code cleanup; update build file

--- xofz.TimeKeeper98/Properties/AssemblyInfo.cs
+++ xofz.TimeKeeper98/Properties/AssemblyInfo.cs
@@ -9,6 +9,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright (c) x(z)")]
 [assembly: ComVisible(false)]
 [assembly: Guid("084c471d-35ef-46d1-b6a3-3d79091feaa8")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
 [assembly: CLSCompliant(true)]
