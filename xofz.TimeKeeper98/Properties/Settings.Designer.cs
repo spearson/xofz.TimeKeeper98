8b1ce395fdeaf4196cb9e7c74151c69c6f218e8d
Sam Pearson <xofz@outlook.com>
2023-12-21 22:45:34 -0800

    Mostly empty commit for new Core98

--- xofz.TimeKeeper98/Properties/Settings.Designer.cs
+++ xofz.TimeKeeper98/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace xofz.TimeKeeper98.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.8.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -52,7 +52,7 @@ namespace xofz.TimeKeeper98.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1")]
+        [global::System.Configuration.DefaultSettingValueAttribute("8")]
         public int TimerIntervalSeconds {
             get {
                 return ((int)(this["TimerIntervalSeconds"]));
